4392edb5d408b793e01cd82957be4465e9cee37c
ariefpratama135@gmail.com <ariefpratama135@gmail.com>
2023-10-26 09:32:51 +0700

    add proxy to /api

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,35 @@
 import { Button, FlexBox, FlexBoxDirection, Icon, Input, Title } from "@ui5/webcomponents-react";
 import "@ui5/webcomponents-icons/dist/account.js"
 import "@ui5/webcomponents-icons/dist/key.js"
+import { useQuery } from "react-query";
+import { useState } from "react";
 
 export default function Login() {
+
+    const { data, refetch } = useQuery(
+        'login',
+        () => {
+            const username = ''
+            const password = ''
+
+            return fetch('/api/auth/login',
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        'username': username,
+                        'password': password
+                    })
+                })
+                .then(res => res.json())
+        },
+        { enabled: false }
+    )
+
+    const onLoginClick = () => {
+        refetch()
+        console.log(data)
+    }
 
     return (
         <FlexBox
@@ -12,20 +39,21 @@ export default function Login() {
             style={{ height: '600px' }}
         >
 
-            <Title style={{marginBottom: '30px'}}>Sign In</Title>
+            <Title style={{ marginBottom: '30px' }}>Sign In</Title>
             <Input
-                style={{marginBottom: '10px'}}
+                style={{ marginBottom: '10px' }}
                 icon={<Icon name="account" />}
                 placeholder="Username"
+                value=""
             />
             <Input
-                style={{marginBottom: '10px'}}
+                style={{ marginBottom: '10px' }}
                 icon={<Icon name="key" />}
                 placeholder="Password"
                 type="Password"
             />
 
-            <Button design="Emphasized" style={{ width: '210px' }}>Login</Button>
+            <Button design="Emphasized" style={{ width: '210px' }} onClick={onLoginClick}>Login</Button>
 
         </FlexBox>
     )
