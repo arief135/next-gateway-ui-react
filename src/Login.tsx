af98fbc2b8f3538ba66294ebcad04ace1f44ba11
ariefpratama135@gmail.com <ariefpratama135@gmail.com>
2023-10-28 08:55:28 +0700

    add login logic

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,11 +1,12 @@
 import { Button, FlexBox, FlexBoxDirection, Icon, Input, Title } from "@ui5/webcomponents-react";
-import "@ui5/webcomponents-icons/dist/account.js"
-import "@ui5/webcomponents-icons/dist/key.js"
 import { useMutation } from "react-query";
-import { useState } from "react";
 import axios from "axios";
 import { useToken } from "./Auth";
-import { Navigate, useNavigate, useNavigation } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js"
+import "@ui5/webcomponents-icons/dist/account.js"
+import "@ui5/webcomponents-icons/dist/key.js"
+import UI5Element from "@ui5/webcomponents-base/dist/UI5Element";
 
 export default function Login() {
 
@@ -17,9 +18,6 @@ export default function Login() {
         return <Navigate to="/" />
     }
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
     const loginMutation = useMutation({
         mutationFn: (loginData: { username: string, password: string }) => axios.post('/api/auth/login', loginData)
     })
@@ -29,37 +27,44 @@ export default function Login() {
         nav('/')
     }
 
-    const onLoginClick = () => {
-        loginMutation.mutate({ username, password })
+    const onLoginClick = (e) => {
+        e.preventDefault()
+
+        const u = document.getElementById('username') as any
+        const p = document.getElementById('password') as any
+        loginMutation.mutate({
+            username: u.value,
+            password: p.value
+        })
     }
 
     return (
-        <FlexBox
-            direction={FlexBoxDirection.Column}
-            alignItems="Center"
-            justifyContent="Center"
-            style={{ height: '600px' }}
-        >
+        <form onSubmit={onLoginClick}>
+            <FlexBox
+                direction={FlexBoxDirection.Column}
+                alignItems="Center"
+                justifyContent="Center"
+                style={{ height: '600px' }}
+            >
 
-            <Title style={{ marginBottom: '30px' }}>Sign In</Title>
-            <Input
-                style={{ marginBottom: '10px' }}
-                icon={<Icon name="account" />}
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <Input
-                style={{ marginBottom: '10px' }}
-                icon={<Icon name="key" />}
-                placeholder="Password"
-                type="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
+                <Title style={{ marginBottom: '30px' }}>Sign In</Title>
+                <Input
+                    id='username'
+                    style={{ marginBottom: '10px' }}
+                    icon={<Icon name="account" />}
+                    placeholder="Username"
+                />
+                <Input
+                    id='password'
+                    style={{ marginBottom: '10px' }}
+                    icon={<Icon name="key" />}
+                    placeholder="Password"
+                    type="Password"
+                />
 
-            <Button design="Emphasized" style={{ width: '210px' }} onClick={onLoginClick}>Login</Button>
+                <Button design="Emphasized" style={{ width: '210px' }} onClick={onLoginClick}>Login</Button>
 
-        </FlexBox>
+            </FlexBox>
+        </form>
     )
 }
